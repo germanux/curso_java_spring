a6528561fe865c070576be2cee34601b13c710d7
German <germanux@gmail.comn>
2019-05-10 16:05:49 +0200

    +

---
+++ 02_Colecciones/src/com/sinensia/poo_avanzando/Depredador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sinensia.poo_avanzando;
+
+/**
+ *
+ * @author Admin
+ */
+// Todos lo métodos son abstractos
+public interface Depredador {
+    // String presa = "";
+    
+    void cazar(String presa);
+}
---
+++ 02_Colecciones/src/com/sinensia/poo_avanzando/Mamifero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sinensia.poo_avanzando;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Mamifero {
+    String mamar(Mamifero mam);
+    String nombrar();
+}
